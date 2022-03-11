54bafa161b95877acb8809094f7111fa82a5648d
Evgeniy Ignashov <eugene.ign@yandex.ru>
2022-03-11 10:23:23 +0500

    Initi message

---
+++ src/main/java/com/devcolibti/database/Main.java
@@ -0,0 +1,13 @@
+package com.devcolibti.database;
+
+import java.sql.Connection;
+import java.sql.Driver;
+
+public class Main {
+    public static void main(String[] args) {
+        Connection connection;
+
+
+
+    }
+}
